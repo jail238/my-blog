015582b2019445969ebbfea2de1c583090054300
jail238 <jail238@naver.com>
2026-06-23 23:51:10 +0900

    Rename blog to M.S.K.

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Global site data.
 
-export const SITE_TITLE = "Hyun's Notes";
+export const SITE_TITLE = "M.S.K.";
 export const SITE_DESCRIPTION =
 	'\uAC1C\uBC1C, \uC218\uD559, \uACF5\uBD80, \uB3C5\uC11C, \uB7EC\uB2DD\uC744 \uAE30\uB85D\uD558\uB294 \uAC1C\uC778 \uBE14\uB85C\uADF8';
 
