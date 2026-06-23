cf360e543064280a7fe1d00773f35e8a67f29488
jail238 <jail238@naver.com>
2026-06-24 00:07:47 +0900

    Refine public blog design

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,7 @@
 // Global site data.
 
 export const SITE_TITLE = "M.S.K.";
-export const SITE_DESCRIPTION =
-	'\uAC1C\uBC1C, \uC218\uD559, \uACF5\uBD80, \uB3C5\uC11C, \uB7EC\uB2DD\uC744 \uAE30\uB85D\uD558\uB294 \uAC1C\uC778 \uBE14\uB85C\uADF8';
+export const SITE_DESCRIPTION = "M.S.K.";
 
 export const CATEGORIES = {
 	development: '\uAC1C\uBC1C \uAE30\uB85D',
