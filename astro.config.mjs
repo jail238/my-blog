a604e67a14a9d3f83b80b413f9418cb3f8c9947c
jail238 <jail238@naver.com>
2026-06-24 14:30:36 +0900

    Render math notation in posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jail238.github.io',
 	base: '/my-blog',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 	fonts: [
 		{
 			provider: fontProviders.local(),
