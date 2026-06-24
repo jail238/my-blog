3ba20e2cc07c4de9553fbbc0db14efee1a5e1bd1
jail238 <jail238@naver.com>
2026-06-24 13:02:45 +0900

    Fix archive copy and about text

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Global site data.
 
 export const SITE_TITLE = "M.S.K.";
-export const SITE_DESCRIPTION = "남는 것은 결국 아카이브";
+export const SITE_DESCRIPTION = "나를 아카이브.";
 
 export const CATEGORIES = {
 	math: '수학',
